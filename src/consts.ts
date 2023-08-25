a962577e69e657150667e61dbe7fd5ba0a8a4066
Robert Halter <roebi@halter.in>
2023-08-26 00:43:49 +0200

    part one: add add bottom of blog the toptics and the GitHub Repo, add use of jsonpath. const with login without pw ! current with const data = { "topics" ... }

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,11 @@
 
 export const SITE_TITLE = 'roebi\'s blog';
 export const SITE_DESCRIPTION = 'Welcome to roebi\'s blog!';
+
+const GITHUB_API_BASE_URL = 'https://api.github.com/';
+const GITHUB_LOGIN = 'roebi';
+export const GITHUB_API_PROJECT_URL = GITHUB_API_BASE_URL + 'repos/' + GITHUB_LOGIN + '/';
+
+// https://github.com/roebi/01-02-Webserver-in-Go-on-Podman
+const GITHUB_HTML_BASE_URL = 'https://github.com/'
+export const GITHUB_OWNER_HTML_URL = GITHUB_HTML_BASE_URL + GITHUB_LOGIN + '/';
