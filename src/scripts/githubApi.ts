9bba82562cd8e9167207e0369518bb986faa47cb
Robert Halter <roebi@halter.in>
2023-08-27 18:52:12 +0200

    add missing return Promise.resolve in return of Mock Data.

--- src/scripts/githubApi.ts
+++ src/scripts/githubApi.ts
@@ -1,6 +1,6 @@
 import jp from 'jsonpath';
 import { GITHUB_API_PROJECT_URL } from '../consts';
-const isProd = import.meta.env.PROD;
+const isProd = false ; // import.meta.env.PROD;
 
 export async function getGithubTopics (githubName: String): Promise<Array<String>> {
   console.info('githubApi.ts: githubName: ' + githubName);
@@ -13,7 +13,7 @@ export async function getGithubTopics (githubName: String): Promise<Array<String
     } else {
       // based on
       // file:///C:/d/r/astro-github-api-plugin/01-01-vanilla-HTML5-starter-page.json
-      return {
+      return Promise.resolve({
         "name": "01-01-vanilla-HTML5-starter-page",
         "html_url": "https://github.com/roebi/01-01-vanilla-HTML5-starter-page",
         "description": "vanilla HTML 5 starter page - Have you ever heard of this HTML 5 tags ?",
@@ -25,7 +25,7 @@ export async function getGithubTopics (githubName: String): Promise<Array<String
           "roebi",
           "starter"
         ]
-      };
+      });
     }
   }
 
