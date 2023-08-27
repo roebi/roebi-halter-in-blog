45b345cbaa9fb3548f7cd313fce2341ae812da74
Robert Halter <roebi@halter.in>
2023-08-27 11:23:16 +0200

    githubApi.ts use of const isProd = import.meta.env.PROD;

--- src/scripts/githubApi.ts
+++ src/scripts/githubApi.ts
@@ -1,29 +1,34 @@
 import jp from 'jsonpath';
 import { GITHUB_API_PROJECT_URL } from '../consts';
+const isProd = import.meta.env.PROD;
 
 export async function getGithubTopics (githubName: String): Promise<Array<String>> {
   console.info('githubApi.ts: githubName: ' + githubName);
 
-  // get from github project api
-  const response = await fetch(GITHUB_API_PROJECT_URL + githubName);
-  const data = await response.json();
-/*
-  // based on
-  // file:///C:/d/r/astro-github-api-plugin/01-01-vanilla-HTML5-starter-page.json
-  const dataMock = {
-	"name": "01-01-vanilla-HTML5-starter-page",
-	"html_url": "https://github.com/roebi/01-01-vanilla-HTML5-starter-page",
-	"description": "vanilla HTML 5 starter page - Have you ever heard of this HTML 5 tags ?",
-	"homepage": "https://roebi.github.io/01-01-vanilla-HTML5-starter-page/",
-	"topics": [
-      "html5",
-      "html5-template",
-      "roebi",
-      "starter"
-    ]
-  };
-  const data = dataMock;
-*/
+  async function returnData (isProd: boolean) {
+    if ( isProd ) {
+      // get from github project api
+      const response = await fetch(GITHUB_API_PROJECT_URL + githubName);
+      return await response.json();
+    } else {
+      // based on
+      // file:///C:/d/r/astro-github-api-plugin/01-01-vanilla-HTML5-starter-page.json
+      return {
+        "name": "01-01-vanilla-HTML5-starter-page",
+        "html_url": "https://github.com/roebi/01-01-vanilla-HTML5-starter-page",
+        "description": "vanilla HTML 5 starter page - Have you ever heard of this HTML 5 tags ?",
+        "homepage": "https://roebi.github.io/01-01-vanilla-HTML5-starter-page/",
+        "topics": [
+          " mockdata!",
+          "html5",
+          "html5-template",
+          "roebi",
+          "starter"
+        ]
+      };
+    }
+  }
+
   // https://www.npmjs.com/package/jsonpath MIT License
-  return jp.query(data, '$.topics[*]') as Array<String>;
+  return jp.query(await returnData(isProd), '$.topics[*]') as Array<String>;
 }
