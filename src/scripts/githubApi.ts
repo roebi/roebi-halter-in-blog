7a39078f1df4e54d87d84c1411cb0781e753f864
Robert Halter <roebi@halter.in>
2023-08-27 11:09:29 +0200

    githubApi.ts : activate get from github project api, without mock data

--- src/scripts/githubApi.ts
+++ src/scripts/githubApi.ts
@@ -1,13 +1,16 @@
 import jp from 'jsonpath';
-// import { GITHUB_API_PROJECT_URL } from '../consts';
+import { GITHUB_API_PROJECT_URL } from '../consts';
 
 export async function getGithubTopics (githubName: String): Promise<Array<String>> {
   console.info('githubApi.ts: githubName: ' + githubName);
-  // const response = await fetch(GITHUB_API_PROJECT_URL + githubName);
-  // const data = await response.json();
+
+  // get from github project api
+  const response = await fetch(GITHUB_API_PROJECT_URL + githubName);
+  const data = await response.json();
+/*
   // based on
   // file:///C:/d/r/astro-github-api-plugin/01-01-vanilla-HTML5-starter-page.json
-  const data = {
+  const dataMock = {
 	"name": "01-01-vanilla-HTML5-starter-page",
 	"html_url": "https://github.com/roebi/01-01-vanilla-HTML5-starter-page",
 	"description": "vanilla HTML 5 starter page - Have you ever heard of this HTML 5 tags ?",
@@ -19,7 +22,8 @@ export async function getGithubTopics (githubName: String): Promise<Array<String
       "starter"
     ]
   };
-
+  const data = dataMock;
+*/
   // https://www.npmjs.com/package/jsonpath MIT License
   return jp.query(data, '$.topics[*]') as Array<String>;
 }
